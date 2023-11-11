fc35cae58897d1ff82f22b4e0ed46a9c5a2bdf55
vsevolod <leto9999@gmail.com>
2023-11-11 11:23:56 +0300

    refactoring

--- user-service/src/users/users.controller.ts
+++ user-service/src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
-    create(@Body() createUserDto: any) {
+    create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
     }
 
